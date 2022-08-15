137f8ca6f09fb560bdd61c2f99520697362382a4
abc <karla@gmail.com>
2022-08-15 16:11:45 -0500

    Inyecccion de dependencias primera parte

---
+++ src/main/java/com/mitocode/repository/PersonaRepoImpl.java
@@ -0,0 +1,17 @@
+package com.mitocode.repository;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.mitocode.DemoConsolaApplication;
+
+public class PersonaRepoImpl implements IPersona{
+	private static Logger LOG=LoggerFactory.getLogger(DemoConsolaApplication.class);
+
+	@Override
+	public void registrar(String nombre) {
+		
+		
+	}
+
+}
